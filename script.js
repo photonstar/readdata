e831f8f424551c5c999915fa9c6be6e581f6b904
svssp <electrophotonstar@gmail.com>
2021-11-22 16:15:23 +0530

    hmm

---
+++ script.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
+import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyD-ZgAugkLCDfgJjI8Igna0hfjGJXKbdyY",
+    authDomain: "instophoton.firebaseapp.com",
+    projectId: "instophoton",
+    storageBucket: "instophoton.appspot.com",
+    messagingSenderId: "4951818018",
+    appId: "1:4951818018:web:ab3039815d44ea7da0af7b",
+    measurementId: "G-YWJBTD9FFS"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+var db = getFirestore();
+const q = query(collection(db, "instadata"));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => { console.log(doc.id, " => ", doc.data()); });
